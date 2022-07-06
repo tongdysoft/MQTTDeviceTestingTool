c9d91f69d6094552f9e378753361a917e14af27e
神楽坂雅詩 <cyscxchope@gmail.com>
2022-07-06 18:55:19 +0800

    监听消息,链接启停,订阅启停

--- i18n.go
+++ i18n.go
@@ -1,13 +1,20 @@
 package main
 
 var langs map[string][]string = map[string][]string{
-	"TITLE":      {"MQTT Server test program", "MQTT 服务器测试程序"},
-	"BOOTING":    {"Starting MQTT server... ( port TCP/1883 )", "正在启动 MQTT 服务器... (端口 TCP/1883 )"},
-	"SERVERFAIL": {"Failed to start MQTT server !", "启动 MQTT 服务器失败！"},
-	"BOOTOK":     {"MQTT server started.", "MQTT 服务器已启动。"},
-	"NEEDSTOP":   {"Received stop request, stopping...", "收到停止请求，正在停止..."},
-	"END":        {"Program run ends.", "程序运行结束。"},
-	// Failed to start MQTT server
+	"TITLE":        {"MQTT Server test program", "MQTT 服务器测试程序"},
+	"BOOTING":      {"Starting MQTT server... ( port TCP/1883 )", "正在启动 MQTT 服务器... (端口 TCP/1883 )"},
+	"SERVERFAIL":   {"Failed to start MQTT server !", "启动 MQTT 服务器失败！"},
+	"BOOTOK":       {"MQTT server started.", "MQTT 服务器已启动。"},
+	"NEEDSTOP":     {"Received stop request, stopping...", "收到停止请求，正在停止..."},
+	"END":          {"Program run ends.", "程序运行结束。"},
+	"CLIENT":       {"Client", "客户端"},
+	"CONNECT":      {"Connected", "已连接"},
+	"DISCONNECT":   {"Disconnected", "断开连接"},
+	"SUBSCRIBED":   {"Subscribed", "开始订阅主题"},
+	"UNSUBSCRIBED": {"Unsubscribed", "取消订阅主题"},
+	"MESSAGE":      {"Received message, From", "收到消息，发件人"},
+	"TOPIC":        {"Topic", "主题"},
+	"PAYLOAD":      {"Payload", "内容"},
 }
 
 func lang(title string) string {
--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	"time"
 
 	mqtt "github.com/mochi-co/mqtt/server"
+	"github.com/mochi-co/mqtt/server/events"
 	"github.com/mochi-co/mqtt/server/listeners"
 	"github.com/mochi-co/mqtt/server/listeners/auth"
 )
@@ -17,16 +18,20 @@ import (
 var language string
 
 func main() {
+	// 初始化启动参数
 	flag.StringVar(&language, "l", "en", "Language")
+
 	flag.Parse()
 	sigs := make(chan os.Signal, 1)
 	done := make(chan bool, 1)
+	// 监听结束信号
 	logPrint("i", lang("TITLE")+" v0.0.1")
 	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
 	go func() {
 		<-sigs
 		done <- true
 	}()
+	// 初始化 MQTT 服务器
 	logPrint("i", lang("BOOTING"))
 	server := mqtt.NewServer(nil)
 	tcp := listeners.NewTCP("t1", ":1883")
@@ -44,9 +49,31 @@ func main() {
 			log.Fatal(err)
 		}
 	}()
-
+	// 有设备连接到服务器
+	server.Events.OnConnect = func(cl events.Client, pk events.Packet) {
+		logPrint("+", fmt.Sprintf("%s %s %s: %+v", lang("CLIENT"), cl.ID, lang("CONNECT"), pk))
+	}
+	// 设备断开连接
+	server.Events.OnDisconnect = func(cl events.Client, err error) {
+		logPrint("-", fmt.Sprintf("%s %s %s: %v", lang("CLIENT"), cl.ID, lang("DISCONNECT"), err))
+	}
+	// 收到订阅请求
+	server.Events.OnSubscribe = func(filter string, cl events.Client, qos byte) {
+		logPrint("+", fmt.Sprintf("%s %s %s %s, (QOS:%v)", lang("CLIENT"), cl.ID, lang("SUBSCRIBED"), filter, qos))
+	}
+	// 收到消息
+	server.Events.OnMessage = func(cl events.Client, pk events.Packet) (pkx events.Packet, err error) {
+		pkx = pk
+		logPrint("i", fmt.Sprintf("%s: %s, %s: %s, %s: %s", lang("MESSAGE"), cl.ID, lang("TOPIC"), string(pkx.TopicName), lang("PAYLOAD"), string(pkx.Payload)))
+		return pk, nil
+	}
+	// 收到取消订阅请求
+	server.Events.OnUnsubscribe = func(filter string, cl events.Client) {
+		logPrint("-", fmt.Sprintf("%s %s %s %s)", lang("CLIENT"), cl.ID, lang("UNSUBSCRIBED"), filter))
+	}
+	// 启动完毕
 	logPrint("i", lang("BOOTOK"))
-
+	// 处理结束信号
 	<-done
 	logPrint("X", lang("NEEDSTOP"))
 	server.Close()
