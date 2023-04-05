7b2ecbd181af10a91f3e2ca1d25aab2f4d7eadfc
神楽坂雅詩 <cyscxchope@gmail.com>
2023-04-05 20:16:09 +0800

    更新第三方库和编译批处理

--- build.bat
+++ build.bat
@@ -1,24 +1,38 @@
-SET name=bin\MqttClientTestTool_v1.1.0_
+SET name=MqttClientTestTool_v1.1.0_
 DEL /Q bin\*
+COPY 中文交互模式.bat bin\
+COPY README.md bin\
+COPY rc.icns bin\app.icns
 SET CGO_ENABLED=0
 SET GOARCH=amd64
 go generate
 SET GOOS=windows
-go build -o %name%Windows64.exe .
+go build -o bin\%name%Windows64.exe .
 DEL /Q *.syso
 SET GOOS=linux
-go build -o %name%Linux64 .
+go build -o bin\%name%Linux64 .
 SET GOOS=darwin
-go build -o %name%macOS64 .
+go build -o bin\%name%macOS64 .
 SET GOARCH=386
 go generate
 SET GOOS=windows
-go build -o %name%Windows32.exe .
+go build -o bin\%name%Windows32.exe .
 DEL /Q *.syso
 SET GOOS=linux
-go build -o %name%Linux32 .
+go build -o bin\%name%Linux32 .
+@REM xz -z -e -9 -T 0 -v bin/*
+cd bin
+7z a -mx9 -tzip %name%Windows.zip %name%Windows* 中文交互模式.bat README.md
+7z a -mx9 -tzip %name%Linux.zip %name%Linux* README.md
+7z a -mx9 -tzip %name%macOS.zip %name%macOS* README.md app.icns
+DEL /Q *32
+DEL /Q *64
+DEL /Q *.exe
+DEL /Q *.bat
+DEL /Q *.md
+DEL /Q *.icns
+cd ..
 SET name=
 SET CGO_ENABLED=
 SET GOARCH=
 SET GOOS=
-xz -z -e -9 -T 0 -v bin/*
--- go.mod
+++ go.mod
@@ -4,12 +4,12 @@ go 1.18
 
 require (
 	github.com/logrusorgru/aurora v2.0.3+incompatible
-	github.com/mochi-co/mqtt v1.2.3
+	github.com/mochi-co/mqtt v1.3.2
 )
 
 require (
 	github.com/akavel/rsrc v0.10.2 // indirect
 	github.com/gorilla/websocket v1.5.0 // indirect
 	github.com/josephspurrier/goversioninfo v1.4.0 // indirect
-	github.com/rs/xid v1.4.0 // indirect
+	github.com/rs/xid v1.5.0 // indirect
 )
